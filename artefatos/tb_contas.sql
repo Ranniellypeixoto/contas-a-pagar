49d198b1e4524f2e54de3682f468e02219862572
Ranniellypeixoto <100246142+Ranniellypeixoto@users.noreply.github.com>
2022-07-27 14:33:48 -0300

    tabela de contas sql

---
+++ artefatos/tb_contas.sql
@@ -0,0 +1,12 @@
+create table contas (
+id int primary key auto_increment,
+descricao varchar(100),
+dataCompetencia DATE,
+dataVencimento DATE,
+valor varchar(10),
+dataPagamento DATE,
+desconto varchar(6),
+juros varchar(3),
+valorPago varchar(10)
+
+);
