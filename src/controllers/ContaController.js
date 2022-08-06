5e6a90955fdbb949bf590c6c56ffb4c677024e28
Ranniellypeixoto <100246142+Ranniellypeixoto@users.noreply.github.com>
2022-08-05 21:14:53 -0300

    exportação da collection contas a pagar

--- src/controllers/ContaController.js
+++ src/controllers/ContaController.js
@@ -1,33 +1,32 @@
-const { visualizar } = require('../services/ContaService');
 const ContaService = require('../services/ContaService');
 
 module.exports = {
     listar: async (request, res) => {
-        const conta = await ContaService.listar(request.body);
-        res.json(conta);
+        const fornecedor = await ContaService.listar(request.body);
+        res.json(fornecedor);
     },
 
     visualizar: async (request, res) => {
         let id = request.params.id;
-        const conta = await ContaService.visualizar(id);
-        res.json(conta);
+        const fornecedor = await ContaService.visualizar(id);
+        res.json(fornecedor);
     },
 
     cadastrar: async (request, res) => {
-        const conta = await ContaService.cadastrar(request.body);
-        res.json(conta);
+        const fornecedor = await ContaService.cadastrar(request.body);
+        res.json(fornecedor);
     },
 
     alterar: async (request, res) => {
         const id = request.params.id;
         const { descricao, dataCompetencia, dataVencimento, valor, dataPagamento, valorPago } = request.body;
-        const conta = await ContaService.alterar(id, descricao, dataCompetencia, dataVencimento, valor, dataPagamento, valorPago);
-        res.json(conta);
+        const fornecedor = await ContaService.alterar(id, descricao, dataCompetencia, dataVencimento, valor, dataPagamento, valorPago);
+        res.json(fornecedor);
         
     },
 
     excluir: async (req, res) => {
-        const conta = await ContaService.excluir(req.params.id);
-        res.json(conta);
+        const fornecedor = await ContaService.excluir(req.params.id);
+        res.json(fornecedor);
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -9,4 +9,12 @@ router.post('/contas', ContaController.cadastrar);
 router.put('/contas/:id', ContaController.alterar);
 router.delete('/contas/:id', ContaController.excluir);
 
+const FornecedoresController = require('./controllers/FornecedoresController');
+
+router.get('/fornecedores', FornecedoresController.listar);
+router.get('/fornecedores/:id', FornecedoresController.visualizar);
+router.post('/fornecedores', FornecedoresController.cadastrar);
+router.put('/fornecedores/:id', FornecedoresController.alterar);
+router.delete('/fornecedores/:id', FornecedoresController.excluir);
+
 module.exports = router;
